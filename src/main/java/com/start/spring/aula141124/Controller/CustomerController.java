dfc3776ee9529cf0de7ad1d870490141ccc72549
FR-macedo <macedo270701@gmail.com>
2024-11-14 14:08:31 -0300

    feat<CustomerController>: criei o Controller Customer

---
+++ src/main/java/com/start/spring/aula141124/Controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.start.spring.aula141124.Controller;
+
+import com.start.spring.aula141124.Model.Customer;
+import com.start.spring.aula141124.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer;
+        savedCustomer = customerRepository.save(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+}
