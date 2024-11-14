ae5b25284b9d96a02bd29f7060128ca630efa7dd
FR-macedo <macedo270701@gmail.com>
2024-11-14 15:11:36 -0300

    edit<Product>: editei o Product para ter Getters e Setters de ID

--- src/main/java/com/start/spring/aula141124/Entities/Product.java
+++ src/main/java/com/start/spring/aula141124/Entities/Product.java
@@ -27,6 +27,14 @@ public class Product {
                 '}';
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
