1436d71923af9de8de9cadb4e6a369c98599e154
FR-macedo <macedo270701@gmail.com>
2024-11-14 14:23:39 -0300

    edit<Costumer>: mudei os atributos

--- src/main/java/com/start/spring/aula141124/Entities/Customer.java
+++ src/main/java/com/start/spring/aula141124/Entities/Customer.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.List;
+
 @Entity
 public class Customer{
     @Id
@@ -12,6 +14,7 @@ public class Customer{
     private Long id;
     private String name;
     private String email;
+    private List<Order> orderList;
 
 
     public Customer(String name, String email) {
