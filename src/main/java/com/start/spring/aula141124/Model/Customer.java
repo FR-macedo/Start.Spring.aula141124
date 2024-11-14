b4382745474eca9361dfb7eebe12b7bf3be0b674
FR-macedo <macedo270701@gmail.com>
2024-11-14 14:07:37 -0300

    feat<CustomerEntity>: criei a entidade Customer

---
+++ src/main/java/com/start/spring/aula141124/Model/Customer.java
@@ -0,0 +1,52 @@
+package com.start.spring.aula141124.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    // Getters and setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Customer() {
+    }
+
+}
